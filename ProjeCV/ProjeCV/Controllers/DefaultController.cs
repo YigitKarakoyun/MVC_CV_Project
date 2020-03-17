f5cb0b62b8050c8162c4a85d74ce7d6a8313a38d
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-03-17 20:12:24 +0300

    cs

--- ProjeCV/ProjeCV/Controllers/DefaultController.cs
+++ ProjeCV/ProjeCV/Controllers/DefaultController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ProjeCV.Models.Entity;
+using ProjeCV.Models.Sinif;
 namespace ProjeCV.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        DbMvcCvEntities db = new DbMvcCvEntities();
         public ActionResult Index()
         {
-            return View();
+            Class1 cs = new Class1();
+            cs.Deger1 = db.TBL_ABOUT.ToList();
+            cs.Deger2 = db.TBL_EXPERIENCE.ToList();
+            cs.Deger3 = db.TBL_EDUCATION.ToList();
+
+            return View(cs);
+            //var degerler = db.TBL_ABOUT.ToList();
+            //return View(degerler);
         }
     }
 }
