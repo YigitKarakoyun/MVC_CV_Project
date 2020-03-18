beaa8fafc765ed136d713579e81f0c8da433526c
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-03-18 15:41:11 +0300

    cs

---
+++ ProjeCV/ProjeCV/Controllers/DeneyimlerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjeCV.Models.Entity;
+using ProjeCV.Models.Sinif;
+namespace ProjeCV.Controllers
+{
+    public class DeneyimlerController : Controller
+    {
+        // GET: Deneyimler
+        DbMvcCvEntities db = new DbMvcCvEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger2 = db.TBL_EXPERIENCE.ToList();
+            return View(cs);
+        }
+    }
+}
---
+++ ProjeCV/ProjeCV/Controllers/EgitimController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjeCV.Models.Entity;
+using ProjeCV.Models.Sinif;
+namespace ProjeCV.Controllers
+{
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        DbMvcCvEntities db = new DbMvcCvEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger3 = db.TBL_EDUCATION.ToList();
+            return View(cs);
+        }
+    }
+}
