b87d13b0c356dfe8cfc5ba006f45d2c6d5ca053d
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-03-20 17:32:49 +0300

    cs

--- ProjeCV/ProjeCV/Controllers/HakkimdaController.cs
+++ ProjeCV/ProjeCV/Controllers/HakkimdaController.cs
@@ -17,5 +17,25 @@ namespace ProjeCV.Controllers
             cs.Deger1 = db.TBL_ABOUT.ToList();
             return View(cs);
         }
+
+        public ActionResult VeriGetir(int id)
+        {
+            var temp = db.TBL_ABOUT.Find(id);
+            return View("VeriGetir", temp);
+        }
+
+        public ActionResult Guncelle(TBL_ABOUT p)
+        {
+            TBL_ABOUT degerler = db.TBL_ABOUT.Find(p.ID);
+            degerler.NAME = p.NAME;
+            degerler.SURNAME = p.SURNAME;
+            degerler.PHONE = p.PHONE;
+            degerler.MAIL = p.MAIL;
+            degerler.ADDRESS = p.ADDRESS;
+            degerler.ABOUT = p.ABOUT;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
